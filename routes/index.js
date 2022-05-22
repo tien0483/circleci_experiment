f587b3a202df7d9d2abc722584f980bb0039f5a0
TienNguyenAkselos <tien.nguyen@akselos.com>
2022-05-22 21:37:41 +0700

    testing

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'CircleCI experimental project.' });
+  res.render('index', { title: 'VnPro CICD Testing' });
 });
 
 module.exports = router;
